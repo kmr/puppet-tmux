fa207ab4dafe9b413240828b3aeb4190d192c962
mio <kmr.clover@gmail.com>
2013-04-12 07:38:25 +0900

    fix shell resource setting

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,19 +7,20 @@ class tmux {
   include homebrew
 
   package { 'tmux': }
-  case $osfamily {
-    'Darwin' => {
+  case $::osfamily {
+    'Darwin': {
       package { 'reattach-to-user-namespace':
       }
 
       file { "${boxen::config::bindir}/tmux-pbcopy.sh":
+        mode   => '655',
         source => 'puppet:///modules/tmux/darwin/tmux-pbcopy.sh',
       }
-    },
-    default  => {
+    }
+    default: {
       package { 'xsel':
         ensure => present,
       }
-    },
+    }
   }
 }
